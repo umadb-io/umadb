0d43e092edda8cd45e6db9f265b8df22a02ba64c
johnbywater <john.bywater@appropriatesoftware.net>
2026-05-11 21:50:05 +0100

    Derive `Default` for `PageID`, `Tsn`, and `Position` structs.

--- umadb-core/src/common.rs
+++ umadb-core/src/common.rs
@@ -1,9 +1,9 @@
 // NewType definitions
-#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash, Default)]
 pub struct PageID(pub u64);
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash, Default)]
 pub struct Tsn(pub u64);
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash, Default)]
 pub struct Position(pub u64);
